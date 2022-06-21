156f596500726669d8585f4f721d01c9c97625e3
Felix Olenko <0feliks0@gmail.com>
2022-06-21 10:35:57 +0300

    3

---
+++ TASK3/Program.cs
@@ -0,0 +1,12 @@
+﻿void setArray()
+{
+    int[] arrA = new int[8];
+    int i = 0;
+    for (i = 0; i < 8; i++)
+    {
+        Random rnd = new Random();
+        arrA[i] = rnd.Next(100,1000);
+        Console.Write($" {arrA[i]} |");
+    }
+}
+setArray();
